1427b77202454d262ced1f631b4407142b15884a
vmurali100 <vmurali100@users.noreply.github.com>
2023-03-01 12:05:52 +0530

    Day 2 Angular

--- Angular/app1-intro/src/app/app.module.ts
+++ Angular/app1-intro/src/app/app.module.ts
@@ -1,16 +1,14 @@
+// Ipmort
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Configuration
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent], 
+  imports: [BrowserModule], // What are the dependencies required for this Module
+  bootstrap: [AppComponent], // Which Compoent has to Render first
 })
-export class AppModule { }
+// Exporting
+export class AppModule {}
